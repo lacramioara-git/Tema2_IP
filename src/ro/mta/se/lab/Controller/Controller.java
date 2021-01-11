c93387acca945fb1d4278be45719f282a7fe9725
lacramioara <runcan_lacramioara@yahoo.com>
2021-01-11 20:00:14 +0200

    Am rezolvat problema afisarii aceluiasi mesaj la apasarea multipla a butonului.Am folosit un alert box de tip INFORMATION
    Signed-off-by: lacramioara <runcan_lacramioara@yahoo.com>

--- src/ro/mta/se/lab/Controller/Controller.java
+++ src/ro/mta/se/lab/Controller/Controller.java
@@ -7,15 +7,14 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import org.json.simple.parser.ParseException;
 import ro.mta.se.lab.Model.*;
 
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 
@@ -36,7 +35,7 @@ public class Controller implements Initializable {
     public Controller() {
 
         WeatherAPI = new WeatherAPIClass();
-        CityWeather = new CityWeather();
+
         MyFileReader=new ReadInitialDataClass();
         CountryId = new ChoiceBox();
         CityId = new ChoiceBox();
@@ -51,14 +50,16 @@ public class Controller implements Initializable {
     }
 
 
-    public void onPressWeatherButton(ActionEvent actionEvent) throws ParseException, IOException {
+    public void onPressWeatherButton() throws ParseException, IOException {
         String response;
-        String City= (String) CityId.getValue();
-        WeatherAPI.setConnection(City,"metric","en");
-        response=WeatherAPI.getResponse();
+        String City = (String) CityId.getValue();
+        WeatherAPI.setConnection(City, "metric", "en");
+        response = WeatherAPI.getResponse();
+        CityWeather = new CityWeather();
         CityWeather.setParameters(response);
-        String title= City + " Weather";
-        WeatherId.setOnAction(e-> AlertBox.displayWeather(title,CityWeather.display()));
-
+        String title = City + " Weather";
+        String exitmsg="I hope it helped. Have a nice day!";
+        AlertBox alert=new AlertBox();
+        alert.displayWeather(title,CityWeather.display(),exitmsg);
     }
 }
